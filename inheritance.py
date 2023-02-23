0c69a4f5b73ba8be014344930d8a1108614eb378
Aminah <unsa3436@ftu.ac.th>
2023-02-23 09:07:28 +0700

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -14,16 +14,6 @@ class Student(User):          #create a class called student and inherit propert
         self.email= email
         self.major= major
         User.__init__(self, name, id, phonenum)
-<<<<<<< HEAD
-
-    def set_studentmark (self, miniProjectMark, finalProjectMark, attendancemark,exersicesDiscussionMark):
-        self.studentmark = miniProjectMark
-        self.studentmark = finalProjectMark
-        self.studentmark = attendancemark
-        self.studentmark = exersicesDiscussionMark
-        print(f"{self.name} has set {Student.name}'s marks {Student.}  {Student.grade}")
-    
-=======
         self.marks = {}
 
     def add_marks(self, subject, marks):
@@ -35,7 +25,6 @@ class Student(User):          #create a class called student and inherit propert
         else:
             print(f"{self.name} has not received any marks for {subject} yet.")
         
->>>>>>> 569eb1fb2b745453d235ec54760bcb3a1c65ab72
         User.show_details(self) #this is the first call for show_detail() method
         print(f"{self.name} got {self.marks}") #this print studentName got studentGrade
     # add method to view a subject's marks (miniProjectMark, finalProjectMark, attendance mark, exersicesDiscussionMark)
